76064279a17d47db12e7debb47055f4dba373a40
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-11-04 01:52:09 +0530

    Create 35_SearchInsertPosition.java

---
+++ Medium/35_SearchInsertPosition.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int s=0,e=nums.length-1;
+        int ans=-1;
+        while(s<=e)
+        {
+            int mid=(s+e)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        return s;
+    }
+}
