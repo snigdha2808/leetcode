f8ca5a7e0daf66cb48a9ee77dd1f26a160abd0d1
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-11-20 03:03:15 +0530

    Create 412_FizzBuzz.java

---
+++ easy/412_FizzBuzz.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int len=String.valueOf(n).length();
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        { 
+            if(i%3==0 && i%5==0)
+            {
+                list.add("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                list.add("Fizz");
+            }
+            else if(i%5==0)
+            {
+                list.add("Buzz");
+            }
+            else
+            {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
