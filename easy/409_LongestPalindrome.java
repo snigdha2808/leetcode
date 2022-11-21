d62bf649dd5bbb16d492e9391e7a2d3ece7b3fe9
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-11-21 19:37:31 +0530

    Create 409_LongestPalindrome.java

---
+++ easy/409_LongestPalindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> set=new HashSet<>();
+        int len = 0;
+        for(char c:s.toCharArray())
+        {
+            if(set.contains(c))
+            {
+                len += 2;
+                set.remove(c);
+            }
+            else
+            {
+                set.add(c);
+            }
+        }
+        if(set.size()>0)
+        {
+            return len+1;
+        }
+        else
+        {
+            return len;
+        }
+    }
+}
