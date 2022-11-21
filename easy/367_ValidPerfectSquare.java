4fb77c218b08bc393b649c7a27cdfef1b4424d8c
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-11-21 19:38:47 +0530

    Create 367_ValidPerfectSquare.java

---
+++ easy/367_ValidPerfectSquare.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long i=1,j=num;
+        while(i<=j)
+        {
+            long mid=i+(j-i)/2;
+            if(mid*mid==num)
+            {
+                return true;
+            }
+            else if(mid*mid<num)
+            {
+                i=mid+1;
+            }
+            else
+            {
+                j=mid-1;
+            }
+        }
+        return false;
+    }
+}
